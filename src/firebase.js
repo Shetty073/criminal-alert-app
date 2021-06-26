c2e639c390e9b2b6c87220d84f31b46dea668c78
Ashish Shetty <30448930+Shetty073@users.noreply.github.com>
2021-06-26 20:51:04 +0530

    add note regarding new admin user registration

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,15 @@
 import firebase from "firebase";
 import 'firebase/auth';
 
+// ! 🚨 NOTE 🚨: Right now we are registering new admin users via 
+// ! createUserWithEmailAndPassword() method. This method automatically
+// ! signs the user in. But we do not want to do that because we are 
+// ! already logged in ourselves and we just want to create other admin 
+// ! users if needed.
+// ! At the time of writing this, the only way to do that is to use the
+// ! firebase-admin SDK, which I cam not going to use but you can if you
+// ! want.
+
 const app = firebase.initializeApp({
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
